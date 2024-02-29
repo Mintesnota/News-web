c4721ae3547d86ab9f4aa2783c44a4399803ad04
Mintesnot Admasu <minte.enguye@gmail.com>
2024-02-29 14:14:28 +0300

    Update about.blade.php

--- resources/views/client/about.blade.php
+++ resources/views/client/about.blade.php
@@ -4,43 +4,26 @@
         <div class="col-md-10 offset-md-1 ">
             <div class="card" data-aos="fade-up">
                 <div class="card-body">
-                  <div class="about us-wrapper">
+                  <div class=" about us-wrapper">
                     <h1 class="mt-5">
                       About Us
                     </h1>
                     <p class="font-weight-600 fs-15">
-                      He has led a remarkable campaign, defying the traditional
-                      mainstream parties courtesy of his En Marche! movement.
-                      For many, however, the campaign has become less about
-                      backing Macron and instead about voting against Le Pen,
-                      the National Front candidate.
-                    </p>
-                    <p class="font-weight-600 fs-15 mb-5 mt-4">
-                      He has led a remarkable campaign, defying the traditional
-                      mainstream parties courtesy of his En Marche! movement.
-                      For many, however, the campaign has become less about
-                      backing Macron and instead about voting against Le Pen,
-                      the National Front candidate.
-                    </p>
-                    <img
-                      src="../assets/images/about/about.jpg"
-                      alt="banner"
-                      class="img-fluid mb-5"
-                    />
-        
-                    <p class="font-weight-600 fs-15 text-center">
-                      He has led a remarkable campaign, defying the traditional
-                      mainstream parties courtesy of his En Marche! movement.
-                      For many, however, the campaign has become less about
-                      backing Macron and instead about voting against Le Pen,
-                      the National Front candidate.
-                    </p>
-                    <p class="font-weight-600 fs-15 mb-5 mt-4 text-center">
-                      He has led a remarkable campaign, defying the traditional
-                      mainstream parties courtesy of his En Marche! movement.
-                      For many, however, the campaign has become less about
-                      backing Macron and instead about voting against Le Pen,
-                      the National Front candidate.
+                        Micro Sun and Solutions plc have been formed by two individual professionals having vivid experience and wide exposure to Information Technology. 
+                        People involved here are also young, qualified business graduates and qualified engineers from renowned universities across the globe. 
+                        MSS began its business operation as a hardware and network solutions-providing company in January 1997.
+
+                       Desire for Excellence
+                       Trust and confidence build up
+                        Innovation
+                        Transparency
+                        Teamwork
+                        MSS provides a stop-automated solution for your trade and industry. Depending on the size and field of your organization,
+                        we have different products and services to meet your requirements. We provide the optimum and customized solutions for your organization.
+
+                        MSS provides one one-stop automated solution for your trade and industry. Depending on the size and field of your organization,
+                        we have different products and services to meet your requirements. We provide the optimum and customized solutions for your organization.
+                  .
                     </p>
                   </div>
                 </div>
